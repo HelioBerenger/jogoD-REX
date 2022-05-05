86a83d60872bd3c146a5b74f6d4aa867ec281021
HelioBerenger <103533359+HelioBerenger@users.noreply.github.com>
2022-05-05 16:04:28 -0300

    agra tem cactu

--- sketch.js
+++ sketch.js
@@ -19,10 +19,17 @@ var groundImage;
 var chaoInvisivel
 var chao;
 var cloudImg
+var cactu1,cactu2,cactu3,cactu4,cactu5,cactu6
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png")
   cloudImg=loadImage("cloud.png")
+  cactu1=loadImage("obstacle1.png")
+  cactu2=loadImage("obstacle2.png")
+  cactu3=loadImage("obstacle3.png")
+  cactu4=loadImage("obstacle4.png")
+  cactu5=loadImage("obstacle5.png")
+  cactu6=loadImage("obstacle6.png")
 }
 var cloud
 function spawnClouds(){
@@ -34,10 +41,28 @@ cloud=createSprite(600,100,40,10)
   cloud.y=Math.round(random(10,60))
   cloud.depth=trex.depth
   trex.depth+=1
+  cloud.lifetime=95
   }
   
 }
 
+function spawnCactus(){
+  if (frameCount%59==0){
+    var cactus=createSprite(600,165,10,40);
+    cactus.velocityX=-7
+      var numero=Math.round(random(1,6))
+      switch(numero) 
+      { case 1: cactus.addImage(cactu1); 
+        break; case 2: 
+        cactus.addImage(cactu2); 
+        break; case 3: cactus.addImage(cactu3); 
+        break; case 4: cactus.addImage(cactu4); 
+        break; case 5: cactus.addImage(cactu5); 
+        break; case 6: cactus.addImage(cactu6); 
+        break; default: break; }
+
+  }
+}
 
 function draw(){
   //definir a cor do plano de fundo 
@@ -60,4 +85,5 @@ function draw(){
     chao.x=chao.width/2
   }
   spawnClouds();
+  spawnCactus();
 }
