53b4190cc3389e28d4bf9f9199994d68f0918c12
HelioBerenger <103533359+HelioBerenger@users.noreply.github.com>
2022-05-19 16:12:30 -0300

    consertei bugs e adicionei uma  tela de perde =>

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ function setup(){
   //criando o trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
+  trex.addAnimation("trex_collided", trex_collided);
   edges = createEdgeSprites();
   
   //adicione dimensão e posição ao trex
@@ -15,6 +16,16 @@ function setup(){
   chaoInvisivel.visible=false
   cactusGroup=new Group()
   cloudsGroup=new Group()
+  //trex.debug=true
+  trex.setCollider("circle", 0, 0, 40);
+  gameOver = createSprite(300, 100);
+  restart = createSprite(300, 140);
+  gameOver.addImage(gameOverImg);
+  restart.addImage(restartImg);
+  restart.scale=0.5
+  gameOver.scale=0.5
+  restart.visible=false
+  gameOver.visible=false
 }
 var trex, trex_running, edges;
 var groundImage;
@@ -26,6 +37,8 @@ var cactusGroup,cloudsGroup;
 var PLAY=1;
 var END=0;
 var gameState=PLAY;
+var trex_collided
+var gameOver, restartImg, gameOverImg
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
@@ -37,19 +50,22 @@ function preload(){
   cactu4=loadImage("obstacle4.png")
   cactu5=loadImage("obstacle5.png")
   cactu6=loadImage("obstacle6.png")
+  trex_collided=loadImage("trex_collided.png")
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
 }
 var cloud
 function spawnClouds(){
   if (frameCount%59==0){
-cloud=createSprite(600,100,40,10)
-  cloud.addImage(cloudImg)
-  cloud.velocityX=-7
-  cloud.scale=random(0.5,0.9)
-  cloud.y=Math.round(random(10,60))
-  cloud.depth=trex.depth
-  trex.depth+=1
-  cloud.lifetime=95
-  cloudsGroup.add(cloud);
+    cloud=createSprite(600,100,40,10)
+    cloud.addImage(cloudImg)
+    cloud.velocityX=-7
+    cloud.scale=random(0.5,0.9)
+    cloud.y=Math.round(random(10,60))
+    cloud.depth=trex.depth
+    trex.depth+=1
+    cloud.lifetime=95
+    cloudsGroup.add(cloud);
   }
   
 }
@@ -96,6 +112,12 @@ function draw(){
     chao.velocityX=0
     cloudsGroup.setVelocityXEach(0);
     cactusGroup.setVelocityXEach(0);
+    trex.velocityY=0;
+    cactusGroup.setLifetimeEach(-1)
+    cloudsGroup.setLifetimeEach(-1)
+    trex.changeAnimation("trex_collided",trex_collided)
+    restart.visible=true
+    gameOver.visible=true
   }
   //registrando a posição y do trex
   
